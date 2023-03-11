9a55e868e28bceeb39eb284f0d87bc27da91335d
Vanessa <98292181+wvvva@users.noreply.github.com>
2023-03-11 22:25:28 +0000

    update

---
+++ .next/server/pages/index.js
@@ -0,0 +1,158 @@
+"use strict";
+(() => {
+var exports = {};
+exports.id = 405;
+exports.ids = [405];
+exports.modules = {
+
+/***/ 486:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(997);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(689);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _styles_Home_module_css__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(828);
+/* harmony import */ var _styles_Home_module_css__WEBPACK_IMPORTED_MODULE_4___default = /*#__PURE__*/__webpack_require__.n(_styles_Home_module_css__WEBPACK_IMPORTED_MODULE_4__);
+/* harmony import */ var next_head__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(968);
+/* harmony import */ var next_head__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(next_head__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var _mui_material_Button__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(819);
+/* harmony import */ var _mui_material_Button__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(_mui_material_Button__WEBPACK_IMPORTED_MODULE_3__);
+
+
+
+
+
+const App = ()=>{
+    const propsData = {
+        login: {
+            variant: "outlined",
+            color: "secondary",
+            children: "LET’S GO!",
+            href: "./signin"
+        },
+        login1: {
+            children: "Login",
+            color: "secondary",
+            variant: "outlined",
+            size: "large",
+            href: "./landingdown"
+        }
+    };
+    const untitledArtwork = "/landing/logo.png";
+    const vector = "/landing/vector1.svg";
+    const vector1 = "/landing/vector.svg";
+    return /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.Fragment, {
+        children: [
+            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)((next_head__WEBPACK_IMPORTED_MODULE_2___default()), {
+                children: [
+                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("title", {
+                        children: " Musiac "
+                    }),
+                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("link", {
+                        rel: "icon",
+                        href: "/landing/logo.png"
+                    })
+                ]
+            }),
+            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("div", {
+                className: (_styles_Home_module_css__WEBPACK_IMPORTED_MODULE_4___default().landing),
+                children: /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                    className: (_styles_Home_module_css__WEBPACK_IMPORTED_MODULE_4___default().flexcontainer),
+                    children: [
+                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("img", {
+                            className: (_styles_Home_module_css__WEBPACK_IMPORTED_MODULE_4___default().untitledartwork4),
+                            src: untitledArtwork
+                        }),
+                        /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                            className: (_styles_Home_module_css__WEBPACK_IMPORTED_MODULE_4___default().flexcontainer1),
+                            children: [
+                                /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                                    className: (_styles_Home_module_css__WEBPACK_IMPORTED_MODULE_4___default().catabsolutecontainer),
+                                    children: [
+                                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((_mui_material_Button__WEBPACK_IMPORTED_MODULE_3___default()), {
+                                            className: (_styles_Home_module_css__WEBPACK_IMPORTED_MODULE_4___default().logininstance1),
+                                            ...propsData.login1
+                                        }),
+                                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("span", {
+                                            className: (_styles_Home_module_css__WEBPACK_IMPORTED_MODULE_4___default().meetyour),
+                                            children: "Meet Your"
+                                        }),
+                                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("span", {
+                                            className: (_styles_Home_module_css__WEBPACK_IMPORTED_MODULE_4___default().musaictext),
+                                            children: "MUSAIC"
+                                        }),
+                                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("span", {
+                                            className: (_styles_Home_module_css__WEBPACK_IMPORTED_MODULE_4___default().gettheplaylistjus),
+                                            children: "Get the playlist just for you, with songs fit your preference."
+                                        }),
+                                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((_mui_material_Button__WEBPACK_IMPORTED_MODULE_3___default()), {
+                                            className: (_styles_Home_module_css__WEBPACK_IMPORTED_MODULE_4___default().logininstance),
+                                            ...propsData.login
+                                        })
+                                    ]
+                                }),
+                                /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("img", {
+                                    className: (_styles_Home_module_css__WEBPACK_IMPORTED_MODULE_4___default().vector1),
+                                    src: vector1
+                                }),
+                                /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("img", {
+                                    className: (_styles_Home_module_css__WEBPACK_IMPORTED_MODULE_4___default().vector),
+                                    src: vector
+                                })
+                            ]
+                        })
+                    ]
+                })
+            })
+        ]
+    });
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (App);
+
+
+/***/ }),
+
+/***/ 819:
+/***/ ((module) => {
+
+module.exports = require("@mui/material/Button");
+
+/***/ }),
+
+/***/ 968:
+/***/ ((module) => {
+
+module.exports = require("next/head");
+
+/***/ }),
+
+/***/ 689:
+/***/ ((module) => {
+
+module.exports = require("react");
+
+/***/ }),
+
+/***/ 997:
+/***/ ((module) => {
+
+module.exports = require("react/jsx-runtime");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = __webpack_require__.X(0, [828], () => (__webpack_exec__(486)));
+module.exports = __webpack_exports__;
+
+})();
---
+++ .next/server/pages/landingdown.js
@@ -0,0 +1,155 @@
+"use strict";
+(() => {
+var exports = {};
+exports.id = 694;
+exports.ids = [694];
+exports.modules = {
+
+/***/ 790:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(997);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(689);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _styles_Home_module_css__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(828);
+/* harmony import */ var _styles_Home_module_css__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(_styles_Home_module_css__WEBPACK_IMPORTED_MODULE_3__);
+/* harmony import */ var next_head__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(968);
+/* harmony import */ var next_head__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(next_head__WEBPACK_IMPORTED_MODULE_2__);
+
+
+
+
+const App = ()=>{
+    const ellipse = "/landingdown/ellipse.svg";
+    const group = "/landingdown/group.svg";
+    const logo = "/landingdown/logo.svg";
+    const group1 = "/landingdown/group1.svg";
+    return /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.Fragment, {
+        children: [
+            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)((next_head__WEBPACK_IMPORTED_MODULE_2___default()), {
+                children: [
+                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("title", {
+                        children: " Musiac "
+                    }),
+                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("link", {
+                        rel: "icon",
+                        href: "/landing/logo.png"
+                    })
+                ]
+            }),
+            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                className: (_styles_Home_module_css__WEBPACK_IMPORTED_MODULE_3___default().landingdown),
+                children: [
+                    /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                        className: (_styles_Home_module_css__WEBPACK_IMPORTED_MODULE_3___default().landingdownflexcontainer),
+                        children: [
+                            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("img", {
+                                className: (_styles_Home_module_css__WEBPACK_IMPORTED_MODULE_3___default().landingimage),
+                                src: group
+                            }),
+                            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("span", {
+                                className: (_styles_Home_module_css__WEBPACK_IMPORTED_MODULE_3___default().landingdowntext1),
+                                children: "MUSIC U Like"
+                            }),
+                            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("span", {
+                                className: (_styles_Home_module_css__WEBPACK_IMPORTED_MODULE_3___default().landingdowntext2),
+                                children: "Pick song features you prefer by using our VIBE PICKER feature to customize your playlist"
+                            })
+                        ]
+                    }),
+                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("img", {
+                        className: (_styles_Home_module_css__WEBPACK_IMPORTED_MODULE_3___default().ellipse),
+                        src: ellipse
+                    }),
+                    /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                        className: (_styles_Home_module_css__WEBPACK_IMPORTED_MODULE_3___default().landingdownflexcontainer),
+                        children: [
+                            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("img", {
+                                className: (_styles_Home_module_css__WEBPACK_IMPORTED_MODULE_3___default().landingimage),
+                                src: logo
+                            }),
+                            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("span", {
+                                className: (_styles_Home_module_css__WEBPACK_IMPORTED_MODULE_3___default().landingdowntext1),
+                                children: "WHEREEVER You Are"
+                            }),
+                            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("span", {
+                                className: (_styles_Home_module_css__WEBPACK_IMPORTED_MODULE_3___default().landingdowntext2),
+                                children: [
+                                    "Link to your SPOTIFY account to enjoy simple but exciting personalized music experience",
+                                    " "
+                                ]
+                            })
+                        ]
+                    }),
+                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("img", {
+                        className: (_styles_Home_module_css__WEBPACK_IMPORTED_MODULE_3___default().ellipse),
+                        src: ellipse
+                    }),
+                    /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                        className: (_styles_Home_module_css__WEBPACK_IMPORTED_MODULE_3___default().landingdownflexcontainer),
+                        children: [
+                            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("img", {
+                                className: (_styles_Home_module_css__WEBPACK_IMPORTED_MODULE_3___default().landingimage),
+                                src: group1
+                            }),
+                            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("span", {
+                                className: (_styles_Home_module_css__WEBPACK_IMPORTED_MODULE_3___default().landingdowntext1),
+                                children: "WHOEVER You’re With"
+                            }),
+                            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("span", {
+                                className: (_styles_Home_module_css__WEBPACK_IMPORTED_MODULE_3___default().landingdowntext2),
+                                children: "Check our FRIEND MATCH feature to get mixed playlist for you and your friend"
+                            })
+                        ]
+                    })
+                ]
+            }),
+            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("span", {
+                className: (_styles_Home_module_css__WEBPACK_IMPORTED_MODULE_3___default().num20238bitsterms),
+                children: "@2023 8BITS Terms of Service Privacy Policy"
+            })
+        ]
+    });
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (App);
+
+
+/***/ }),
+
+/***/ 968:
+/***/ ((module) => {
+
+module.exports = require("next/head");
+
+/***/ }),
+
+/***/ 689:
+/***/ ((module) => {
+
+module.exports = require("react");
+
+/***/ }),
+
+/***/ 997:
+/***/ ((module) => {
+
+module.exports = require("react/jsx-runtime");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = __webpack_require__.X(0, [828], () => (__webpack_exec__(790)));
+module.exports = __webpack_exports__;
+
+})();
