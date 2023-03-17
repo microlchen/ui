8cd361e2c8ec31c24e6e28a333e9f5877e9dc60e
Vanessa <98292181+wvvva@users.noreply.github.com>
2023-03-17 19:01:11 +0000

    mobile test

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "61221fb86fe2ee5f"; }
+/******/ 		__webpack_require__.h = function() { return "9351b8bfa3781edf"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
