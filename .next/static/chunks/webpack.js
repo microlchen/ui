7c862bed62b32003340d083832528a57cc519118
Vanessa <98292181+wvvva@users.noreply.github.com>
2023-03-17 18:50:48 +0000

    dashboard mobile

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d2684eb772643ce6"; }
+/******/ 		__webpack_require__.h = function() { return "a8c7335eb8adf325"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
